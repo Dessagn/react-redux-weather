9e533282ed6b986cf961bf15e1976ce6b452073a
Dessagn <temesgen.dessalegn@gmail.com>
2017-10-30 21:35:11 +0200

    some improvements

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'f73ba613a3b76837f1619a8bf18fd0e0';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
